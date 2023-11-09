9252a26ad70e0637373e19a54f47760a56ec0c6c
Song Yangyu <flyfy1@gmail.com>
2023-11-09 23:08:25 +0800

    support pause task by ID

--- commands/task_pause.go
+++ commands/task_pause.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 	"github.com/todopeer/cli/api"
@@ -11,23 +12,33 @@ import (
 var pauseTaskCmd = &cobra.Command{
 	Use:     "pause",
 	Aliases: []string{"p"},
-	Short:   "pause(p) current running task/event",
+	Short:   "pause(p) current running task/event. If an ID is provided, pause that task instead",
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		token := config.MustGetToken()
 		client := api.NewClient(token)
 
-		// try getting the current running task
-		user, err := client.Me()
-		if err != nil {
-			return err
+		var taskID api.ID
+		if len(args) > 0 {
+			taskIDInt, err := strconv.ParseInt(args[0], 10, 64)
+			if err != nil {
+				return fmt.Errorf("unable to parse taskID: %s, error: %w", args[0], err)
+			}
+			taskID = api.ID(taskIDInt)
+		} else {
+			// try getting the current running task
+			user, err := client.Me()
+			if err != nil {
+				return err
+			}
+
+			if user.RunningTaskID == nil {
+				return ErrNoRunningTaskNeedID
+			}
+
+			taskID = *user.RunningTaskID
 		}
 
-		if user.RunningTaskID == nil {
-			return ErrNoRunningTaskNeedID
-		}
-		taskID := int64(*user.RunningTaskID)
-
-		t, err := client.UpdateTask(api.ID(taskID), api.TaskUpdateInput{
+		t, err := client.UpdateTask(taskID, api.TaskUpdateInput{
 			Status: &api.TaskStatusPaused,
 		})
 		if err != nil {
